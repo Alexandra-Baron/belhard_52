6a2c2fde9021ee960f3d22455063ef3ff9a851da
Alexandra-Baron <aleksandra.baron96@mail.ru>
2022-04-10 14:32:04 +0300

    3-2 commit

---
+++ 3-2.py
@@ -0,0 +1,5 @@
+arg_1 = int(input('введите аргумент 1'))
+arg_2 = int(input('введите аргумент 2'))
+arg_3 = int(input('введите аргумент 3'))
+average = (arg_1 + arg_2 + arg_3) / 3
+print(round(average), 3)
